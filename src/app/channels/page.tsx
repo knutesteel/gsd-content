35420ddbd8fde1845ad34afe1300387e0fb1ce48
knutesteel <knutesteel@gmail.com>
2026-08-19 13:53:41 -0400

    Remove Channels page

--- src/app/channels/page.tsx
+++
@@ -1,2 +0,0 @@
-import { ModulePage } from "@/components/module-page";
-export default async function Page({searchParams}:{searchParams:Promise<{notice?:string}>}) { const p=await searchParams; return <ModulePage module="channel" notice={p.notice}/>; }

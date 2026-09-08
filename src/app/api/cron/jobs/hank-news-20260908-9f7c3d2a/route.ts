3f3b147a4bebf7b50c41bba9f2e84c628f1efd96
knutesteel <knutesteel@gmail.com>
2026-09-08 09:05:33 -0400

    Remove temporary Sep 8 Hank news verification route

--- src/app/api/cron/jobs/hank-news-20260908-9f7c3d2a/route.ts
+++
@@ -1,8 +0,0 @@
-import { GET as ingest } from "@/app/api/automation/hank-news-20260908-9f7c3d2a/route";
-
-export const dynamic = "force-dynamic";
-export const maxDuration = 60;
-
-export async function GET() {
-  return ingest();
-}
